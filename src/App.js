158d713fe4e991006fa4c58534ce5aa19c27933d
LeenMass <leen.msarwy12@gmail.com>
2023-01-28 18:43:59 +0200

    update the files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
-import Projects from "./pages/projects/Projects";
+import Projects from "./pages/projects/index";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home/index";
-import Absence from "./pages/Absence/Absence"
-import Employee from "./pages/Employes/Employee";
+import Absence from "./pages/Absence/index"
+import Employee from "./pages/Employes/index";
 import Sidebar from "./components/Sidebar";
 function App() {
   return (
-    <div style={{textAlign:'center'}}>
-     <Router> 
-       <NavBar/>
-            
-          <Sidebar/>
-         <Routes>
-           <Route path="/" element={<Home/>}/>
-           <Route path="/Projects" element={<Projects />} />
-           <Route path="/Absence" element={<Absence />} />
-           <Route path="/Employes" element={<Employee />} />
-         </Routes>
+    <div style={{ textAlign: 'center' }}>
+
+      <Router>
+        <NavBar />
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Projects" element={<Projects />} />
+          <Route path="/Absence" element={<Absence />} />
+          <Route path="/Employes" element={<Employee />} />
+        </Routes>
       </Router>
     </div>
   )
