c073d8c888ff437e63065715667308d2654a485f
skittan4 <souheilkittan@gmail.com>
2023-02-02 19:16:05 +0200

    fix review coments

--- src/App.js
+++ src/App.js
@@ -1,48 +1,41 @@
-import { BrowserRouter as Router, Routes, Route, BrowserRouter,useMatch} from "react-router-dom";
-import Projects from "./pages/Projects/index";
-import NavBar from "./components/NavBar";
-import Home from "./pages/Home/index";
-import Absence from "./pages/Absence/index"
-import Employee from "./pages/Employes/index";
-import Sidebar from "./components/Sidebar";
-import Login from "./pages/Login/Login";
-import Style from "./style.module.scss"
-import "./reset.scss"
-import "./App.scss";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+  useMatch
+} from 'react-router-dom'
+import Projects from './pages/Projects/index'
+import NavBar from './components/NavBar'
+import Home from './pages/Home/index'
+import Absence from './pages/Absence/index'
+import Employee from './pages/Employes/index'
+import Sidebar from './components/Sidebar'
+import Login from './pages/Login/Login'
+import Style from './style.module.scss'
+import './reset.scss'
+import './App.scss'
 
 function App() {
-  const match= useMatch("/")
-
-  console.log(window.location.pathname)
-
   if (window.location.pathname === '/') {
     return (
-      <div style={{textAlign:'center'}}>
-       <NavBar/>
-          
-           {!match&&<Sidebar/>}
-           <Routes>
-           <Route path="/" element={<Login />} />
-             <Route path="/Home" element={<Home/>}/>
-             <Route path="/Projects" element={<Projects />} />
-             <Route path="/Absence" element={<Absence />} />
-             <Route path="/Employes" element={<Employee />} />
-           </Routes>
-        
+      <div style={{ textAlign: 'center' }}>
+        <NavBar />
+
+        <Login />
       </div>
     )
   }
 
   return (
     <div style={{ textAlign: 'center' }}>
-
       <Router>
         <NavBar />
         <div className={Style.pageBody}>
           <Sidebar />
           <div className={Style.mainPage}>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
               <Route path="/Projects" element={<Projects />} />
               <Route path="/Absence" element={<Absence />} />
               <Route path="/Employes" element={<Employee />} />
@@ -50,7 +43,7 @@ function App() {
           </div>
         </div>
       </Router>
-    </div >
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
-import 'primereact/resources/themes/lara-light-indigo/theme.css';   // theme
-import 'primereact/resources/primereact.css'; 
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+import 'primereact/resources/themes/lara-light-indigo/theme.css' // theme
+import 'primereact/resources/primereact.css'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Router> <App /></Router>
-   
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 )
